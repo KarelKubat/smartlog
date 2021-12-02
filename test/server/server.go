d6b3bb3cedbf06c7b2d9c51f25356bc7f6c2d992
Karel Kubat <karel@kubat.nl>
2021-12-03 00:16:38 +0100

    usage and flag -s added

--- test/server/server.go
+++ test/server/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"smartlog/client"
 	anyclient "smartlog/client/any"
@@ -10,12 +13,20 @@ import (
 	"smartlog/server"
 )
 
-func main() {
-	if len(os.Args) <= 2 {
-		fmt.Fprintln(os.Stderr, "Supply one server URI and one or more client URIs, e.g. `udp://:2021 file://stdout`")
-		os.Exit(1)
-	}
+const usage = `
+Usage: server [FLAGS] SERVERADDRESS CLIENTS
+SERVERADDRESS defines what the server listens to and must be in the form:
+  udp://HOSTNAME:PORT (leave out the HOSTNAME to listen to all available addresses), or
+  tcp://HOSTNAME:PORT (again, the HOSTNAME can be left out)
+CLIENTS defines where received messages are fanned out to:
+  udp://HOSTNAME:PORT or tcp://HOSTNAME:PORT forwards over the network
+  file://stdout dumps to stdout, file://FILENAME overwrites FILENAME and dumps in there
+  none://WHATEVER discards, useful for testing
+FLAGS may be:
+  -s DURATION: stops the server after the stated duration, useful for testing
+`
 
+func main() {
 	var err error
 	defer func() {
 		if err != nil {
@@ -24,12 +35,26 @@ func main() {
 		}
 	}()
 
+	flagS := flag.Duration("s", 0, "stop server after stated duration, 0 = serve forever")
+	flag.Parse()
+	if flag.NArg() < 2 {
+		err = errors.New(usage)
+		return
+	}
+
 	var srv *server.Server
-	srv, err = server.New(os.Args[1])
+	srv, err = server.New(flag.Arg(0))
 	if err != nil {
 		return
 	}
-	for _, uri := range os.Args[2:] {
+	if *flagS > 0 {
+		go func() {
+			time.Sleep(*flagS)
+			srv.Close()
+		}()
+	}
+
+	for _, uri := range flag.Args()[1:] {
 		var cl *client.Client
 		cl, err = anyclient.New(uri)
 		if err != nil {
