4dd70461f0becc0ba5fff0ecdd24c1f94f29c653
Karel Kubat <karel@kubat.nl>
2021-12-03 22:43:56 +0100

    tcp/udp server restarts implemented with backoff, preparing for dropping less important messages

--- server/server.go
+++ server/server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"strings"
 	"sync"
+	"time"
 
 	"smartlog/client"
 	"smartlog/linebuf"
@@ -13,7 +14,11 @@ import (
 )
 
 const (
-	chSize = 1024 // # of messages that may be buffered while fanning out
+	// chSize = 1024 // # of messages that may be buffered while fanning out
+	chSize          = 20                // # of messages that may be buffered while fanning out
+	dropInfo        = chSize * 100 / 75 // when to start dropping networke-bound Info(f) messages
+	restartWait     = time.Second / 10  // waittime between listener restarts
+	restartAttempts = 10                // # of restart attempts
 )
 
 type Server struct {
@@ -22,6 +27,7 @@ type Server struct {
 	bufCh       chan []byte      // msg channel for fanout to clients
 	tcpListener net.Listener     // in the case of a TCP server
 	udpConn     *net.UDPConn     // in the case of a UDP server
+	closed      bool             // true upon server.Close()
 }
 
 func New(u string) (*Server, error) {
@@ -39,18 +45,12 @@ func New(u string) (*Server, error) {
 	// Set the connection
 	switch ur.Scheme {
 	case uri.TCP:
-		s.tcpListener, err = net.Listen(fmt.Sprintf("%v", ur.Scheme), strings.Join(ur.Parts, ":"))
-		if err != nil {
-			return nil, fmt.Errorf("%v: failed to start TCP listener: %v", s, err)
+		if err := s.tcpStartListener(); err != nil {
+			return nil, err
 		}
 	case uri.UDP:
-		addr, err := net.ResolveUDPAddr(fmt.Sprintf("%v", ur.Scheme), strings.Join(ur.Parts, ":"))
-		if err != nil {
-			return nil, fmt.Errorf("%v: failed to resolve address: %v", s, err)
-		}
-		s.udpConn, err = net.ListenUDP(fmt.Sprintf("%v", ur.Scheme), addr)
-		if err != nil {
-			return nil, fmt.Errorf("%v: failed to start UDP listener: %v", s, err)
+		if err := s.udpStartListener(); err != nil {
+			return nil, err
 		}
 	default:
 		return nil, fmt.Errorf("%v: only udp:// or tcp:// servers are supported", s)
@@ -74,16 +74,22 @@ func (s *Server) Serve() error {
 
 	switch s.URI.Scheme {
 	case uri.TCP:
-		s.tcpServe()
+		if err := s.tcpServe(); err != nil {
+			return fmt.Errorf("%v: TCP server stopped: %v", s, err)
+		}
 	case uri.UDP:
-		s.udpServe()
+		if err := s.udpServe(); err != nil {
+			return fmt.Errorf("%v: UDP server stopped: %v", s, err)
+		}
 	default:
 		errors.New("internal foobar, unhandled case in server.Serve")
 	}
-	return fmt.Errorf("%v: server stopped", s)
+	return nil
 }
 
 func (s *Server) Close() error {
+	s.closed = true
+
 	var err error
 	switch s.URI.Scheme {
 	case uri.TCP:
@@ -94,23 +100,46 @@ func (s *Server) Close() error {
 	return err
 }
 
-func (s *Server) udpServe() {
+func (s *Server) udpStartListener() error {
+	var err error
+	var addr *net.UDPAddr
+
+	for i := 0; i < restartAttempts; i++ {
+		time.Sleep(restartWait * time.Duration(i))
+		addr, err = net.ResolveUDPAddr(s.URI.Scheme.String(), strings.Join(s.URI.Parts, ":"))
+		if err != nil {
+			return fmt.Errorf("%v: failed to resolve address: %v", s, err)
+		}
+		s.udpConn, err = net.ListenUDP(s.URI.Scheme.String(), addr)
+		if err == nil {
+			return nil
+		}
+		if err != nil {
+			return fmt.Errorf("%v: failed to start UDP listener: %v", s, err)
+		}
+	}
+	return fmt.Errorf("%v: failed to start UDP listener: %v", s, err)
+}
+
+func (s *Server) udpServe() error {
 	line := linebuf.New()
 
-	// Don't return unless the connection gets closed.
+	// Don't return unless the server gets closed.
 	for {
 		for {
 			buf := make([]byte, 1024)
 			n, addr, err := s.udpConn.ReadFromUDP(buf)
 			if err != nil {
-				client.Warnf("%v: failed to handle UDP connection from %v: %v", s, addr, err)
-				if strings.Contains(err.Error(), "use of closed network connection") {
-					return
+				if s.closed {
+					return nil
 				}
-				continue
+				client.Warnf("%v: failed to handle UDP connection from %v: %v", s, addr, err)
+				if err := s.udpStartListener(); err != nil {
+					return err
+				}
 			}
 			if n == 0 {
-				return
+				continue
 			}
 			line.Add(buf, n)
 			for line.Complete() {
@@ -120,16 +149,28 @@ func (s *Server) udpServe() {
 	}
 }
 
-func (s *Server) tcpServe() {
+func (s *Server) tcpStartListener() error {
+	var err error
+	for i := 0; i < restartAttempts; i++ {
+		time.Sleep(restartWait * time.Duration(i))
+		s.tcpListener, err = net.Listen(s.URI.Scheme.String(), strings.Join(s.URI.Parts, ":"))
+		if err == nil {
+			return nil
+		}
+	}
+	return fmt.Errorf("%v: failed to start TCP listener: %v", s, err)
+}
+
+func (s *Server) tcpServe() error {
 	// Don't return unless the connection gets closed.
 	for {
 		conn, err := s.tcpListener.Accept()
 		if err != nil {
-			client.Warnf("%v: failed to accept TCP connection: %v", s, err)
-			if strings.Contains(err.Error(), "use of closed network connection") {
-				return
+			if s.closed {
+				return nil
 			}
-			continue
+			client.Warnf("%v: failed to accept TCP connection: %v", s, err)
+			continue // restart listener
 		}
 		go s.handleTCPConnection(conn)
 	}
@@ -170,12 +211,22 @@ func (s *Server) fanout() {
 	// fanout() never returns, but forever consumes messages from the bufCh
 	for {
 		buf := <-s.bufCh
+		// TESTCODE starts
+		// fmt.Println("bufsz:", len(s.bufCh))
+		// time.Sleep(time.Second / 10)
+		// TESTCODE ends
+
+		// TODO:
+		//  reparse the buffer
+		//  if Infotag present && len(s.bufCh) > dropInfo: discard the message
+
 		var wg sync.WaitGroup
 		for _, c := range s.clients {
 			wg.Add(1)
 			go func(c *client.Client, buf []byte) {
 				if err := c.Passthru(buf); err != nil {
-					client.Warnf("%v: failed to fanout to client %v: %v, buf %v", s, c, err, string(buf))
+					client.Warnf("%v: failed to fanout to client %v: %v, buf %v",
+						s, c, err, strings.TrimRight(string(buf), "\n"))
 				}
 				wg.Done()
 			}(c, buf)
