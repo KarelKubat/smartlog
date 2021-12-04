af976f3af24a99adbf9f16a7bd9f6fe12fe712ae
Karel Kubat <karel@kubat.nl>
2021-12-04 12:32:41 +0100

    added unknownTag and TypeFromBytes for reparsing

--- msg/msg.go
+++ msg/msg.go
@@ -1,6 +1,7 @@
 package msg
 
 import (
+	"bytes"
 	"strings"
 	"time"
 )
@@ -8,12 +9,14 @@ import (
 const (
 	DefaultTimeFormat = "2006-01-02 15:04:05"
 
-	debugTag  byte = 'D'
-	infoTag        = 'I'
-	warnTag        = 'W'
-	errorTag       = 'E'
-	separator      = '|'
-	space          = ' '
+	debugTag   byte = 'D' // Tags for Debug(f) etc.
+	infoTag         = 'I'
+	warnTag         = 'W'
+	errorTag        = 'E'
+	unknownTag      = '?' // For reparsing misses
+
+	separator = '|' // Message parts are separated by space, separator, space
+	space     = ' '
 )
 
 type MsgType int
@@ -23,13 +26,23 @@ const (
 	Info
 	Warn
 	Error
+	Unknown
 )
 
 var tagForType = map[MsgType]byte{
-	Debug: debugTag,
-	Info:  infoTag,
-	Warn:  warnTag,
-	Error: errorTag,
+	Debug:   debugTag,
+	Info:    infoTag,
+	Warn:    warnTag,
+	Error:   errorTag,
+	Unknown: unknownTag,
+}
+
+var typeForTag = map[byte]MsgType{
+	debugTag:   Debug,
+	infoTag:    Info,
+	warnTag:    Warn,
+	errorTag:   Error,
+	unknownTag: Unknown,
 }
 
 type Message struct {
@@ -58,4 +71,12 @@ func BytesFromMessage(m *Message) [][]byte {
 		out = append(out, lineBytes)
 	}
 	return out
+}
+
+func TypeFromBytes(msg []byte) MsgType {
+	parts := bytes.Split(msg, []byte{space, separator, space})
+	if len(parts) < 3 || len(parts[1]) != 1 {
+		return Unknown
+	}
+	return typeForTag[parts[1][0]]
 }
