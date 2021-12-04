ff342f2148be6dd30d7ef06bdc79cb708e24a90e
Karel Kubat <karel@kubat.nl>
2021-12-04 21:24:35 +0100

    reconnecting network clients added

--- client/network/network.go
+++ client/network/network.go
@@ -1,10 +1,6 @@
 package network
 
 import (
-	"fmt"
-	"net"
-	"strings"
-
 	"smartlog/client"
 	"smartlog/uri"
 )
@@ -13,11 +9,8 @@ func New(ur *uri.URI) (*client.Client, error) {
 	c := &client.Client{
 		URI: ur,
 	}
-	var err error
-	c.Conn, err = net.Dial(fmt.Sprintf("%v", ur.Scheme), strings.Join(ur.Parts, ":"))
-	if err != nil {
-		return nil, fmt.Errorf("%v: failed to connect: %v", c, err)
+	if err := c.Connect(); err != nil {
+		return nil, err
 	}
-	c.Writer = c.Conn
 	return c, nil
 }
