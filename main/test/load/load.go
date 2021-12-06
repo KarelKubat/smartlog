cc35223c67fc2d9754ad61afdbcac58336b89aba
Karel Kubat <karel@kubat.nl>
2021-12-06 23:54:21 +0100

    args parsing in separate function

--- main/test/load/load.go
+++ main/test/load/load.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"smartlog/client"
@@ -13,15 +14,15 @@ import (
 	"smartlog/server"
 )
 
+var (
+	fThreads  = flag.Int("threads", 1, "nr of parallel client runs")
+	fDuration = flag.Duration("duration", time.Second, "load duration")
+	fServer   = flag.String("server", "udp://:2025", "server to start")
+	fClients  = flag.String("clients", "none://x", "comma-separated list of server clients")
+)
+
 func main() {
-	//	fThreads := flag.Int("threads", 1, "nr of parallel client runs")
-	fDuration := flag.Duration("duration", time.Second, "load duration")
-	fServer := flag.String("server", "udp://:2025", "server to start")
-	fClients := flag.String("clients", "none://x", "comma-separated list of server clients")
-	flag.Parse()
-	if len(flag.Args()) != 1 || flag.Arg(0) != "go" {
-		checkErr(errors.New("Usage: `load [FLAGS] go`\nRun `load -h` to see what flags you may set."))
-	}
+	checkErr(parseCmdLine())
 
 	// Initialize the server, add its clients, start serving, close it when we're done.
 	srv, err := server.New(*fServer)
@@ -42,17 +43,34 @@ func main() {
 	client.DefaultClient, err = any.New(*fServer)
 	checkErr(err)
 
-	// Load until we're out of time.
+	// Start indicated threads.
+	msgsPerThread := make([]int, *fThreads)
 	start := time.Now()
-	msgNr := 0
-	for {
-		if time.Now().Sub(start) > *fDuration {
-			break
-		}
-		msgNr++
-		err = client.Warnf("hello there, this is message # %v", msgNr)
+	var wg sync.WaitGroup
+	for i := 0; i < *fThreads; i++ {
+		wg.Add(1)
+		go func(slot int) {
+			defer wg.Done()
+			// Load until we're out of time.
+			msgNr := 0
+			for {
+				if time.Now().Sub(start) > *fDuration {
+					break
+				}
+				msgNr++
+				checkErr(client.Warnf("hello there, this is message # %v", msgNr))
+			}
+			msgsPerThread[slot] = msgNr
+		}(i)
 	}
-	fmt.Printf("%v messages sent in %v\n", msgNr, time.Now().Sub(start))
+	wg.Wait()
+
+	total := 0
+	for i := 0; i < *fThreads; i++ {
+		fmt.Printf("thread %v sent %v messages\n", i, msgsPerThread[i])
+		total += msgsPerThread[i]
+	}
+	fmt.Printf("average: %v in %v\n", total / *fThreads, *fDuration)
 }
 
 func checkErr(err error) {
@@ -60,4 +78,15 @@ func checkErr(err error) {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
+}
+
+func parseCmdLine() error {
+	flag.Parse()
+	if len(flag.Args()) != 1 || flag.Arg(0) != "go" {
+		return errors.New("Usage: `load [FLAGS] go`\nRun `load -h` to see what flags you may set.")
+	}
+	if *fThreads < 1 {
+		return errors.New("--threads NR: must be a positive number")
+	}
+	return nil
 }
