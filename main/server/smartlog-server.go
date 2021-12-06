0b6c07f50ad1a5f44017fa9f3ae47f8ae4200f26
Karel Kubat <karel@kubat.nl>
2021-12-06 01:08:13 +0100

    usgae info cleanup

--- main/server/smartlog-server.go
+++ main/server/smartlog-server.go
@@ -11,30 +11,39 @@ import (
 	"smartlog/server"
 )
 
-const usage = `
-Usage: smartlog-server [FLAGS] SERVERADDRESS CLIENT(S)
-SERVERADDRESS defines what the server listens to and must be in the form:
-  udp://HOSTNAME:PORT (leave out the HOSTNAME to listen to all available addresses), or
-  tcp://HOSTNAME:PORT (again, the HOSTNAME can be left out)
-CLIENTS defines where received messages are fanned out to. At least one must be given.
-  udp://HOSTNAME:PORT or tcp://HOSTNAME:PORT forwards over the network
-  file://stdout dumps to stdout, file://FILENAME appends to FILENAME
-  none://WHATEVER discards, useful for testing
-FLAGS may be:
+const (
+	version = "0.01"
+	usage   = `
+This is the Smartlog Server, catcher and forwarder of Smartlog-client generated
+messages; version ` + version + `
+
+Usage: smartlog-server [FLAGS] SERVERADDRESS CLIENT [CLIENT...]
+Where:
+
+  SERVERADDRESS defines what the server listens to and must be in the form:
+    udp://HOSTNAME:PORT : (leave out the HOSTNAME to listen to all IPs), or
+    tcp://HOSTNAME:PORT : (again, the HOSTNAME can be left out)
+
+  CLIENTS defines where received messages are fanned out to. At least one must
+  be given. Use one or more of:
+    file://stdout      : dumps to stdout
+    file://FILENAME     : appends to FILENAME
+    tcp://HOSTNAME:PORT : forwards to a next hop over TCP
+    udp://HOSTNAME:PORT : forwards to a next hop over UDP
+    none://WHATEVER     : discards, useful for testing
+
+  FLAGS may be:
 `
+)
 
 func main() {
 	// Supported flag(s)
 	flagS := flag.Duration("s", 0, "stop server after stated duration, 0 = serve forever")
 
-	// usageFunc() shows how to invoke the server
-	usageFunc := func() {
-		fmt.Fprintf(os.Stderr, usage)
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+	// Parse options, show usage when that fails.
 	flag.Usage = usageFunc
 	flag.Parse()
+	// We need at least 2 positional arguments, or show usage and stop.
 	if flag.NArg() < 2 {
 		usageFunc()
 	}
@@ -63,4 +72,10 @@ func checkErr(err error) {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
+}
+
+func usageFunc() {
+	fmt.Fprintf(os.Stderr, usage)
+	flag.PrintDefaults()
+	os.Exit(1)
 }
